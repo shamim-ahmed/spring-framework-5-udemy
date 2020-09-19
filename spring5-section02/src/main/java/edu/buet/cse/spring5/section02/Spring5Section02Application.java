56552bf050b4b63f38cb26dfb0a4374163127f85
Shamim Ahmed <shamim.buet.99@gmail.com>
2020-09-19 16:05:02 +1000

    Add the project structure

---
+++ spring5-section02/src/main/java/edu/buet/cse/spring5/section02/Spring5Section02Application.java
@@ -0,0 +1,12 @@
+package edu.buet.cse.spring5.section02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5Section02Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Spring5Section02Application.class, args);
+  }
+}
---
+++ spring5-section02/src/test/java/edu/buet/cse/spring5/section02/Spring5Section02ApplicationTests.java
@@ -0,0 +1,12 @@
+package edu.buet.cse.spring5.section02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5Section02ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}
